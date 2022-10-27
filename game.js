e80bfd7248a1235c63cfcc7651627caa7bdff0a7
LogExE <awesome149712@gmail.com>
2022-10-27 15:59:27 +0400

    Canvas changes

--- game.js
+++ game.js
@@ -1,73 +1,68 @@
 
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.createElement("canvas");
+canvas.width = 800;
+canvas.height = 600;
+document.body.appendChild(canvas);
 
+const ctx = canvas.getContext("2d");
 ctx.font = "30px Georgia";
 
 const keyboard = new Map();
-function keyDownHandler(ev)
-{
+function keyDownHandler(ev) {
     keyboard.set(ev.keyCode, true);
 }
-function keyUpHandler(ev)
-{
+function keyUpHandler(ev) {
     keyboard.set(ev.keyCode, false);
 }
 window.addEventListener('keydown', keyDownHandler);
 window.addEventListener('keyup', keyUpHandler);
 
-class Player
-{
-    constructor()
-    {
-	this.x = 0;
-	this.y = 0;
-	this.v_x = 0;
-	this.v_y = 0;
+class Player {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.v_x = 0;
+        this.v_y = 0;
     }
-    update()
-    {
-	if (keyboard.get('D'.charCodeAt(0)))
-	    this.v_x = 2;
-	else if (keyboard.get('A'.charCodeAt(0)))
-	    this.v_x = -2;
-	else this.v_x = 0;
-	if (keyboard.get('W'.charCodeAt(0)))
-	    this.v_y = -2;
-	else if (keyboard.get('S'.charCodeAt(0)))
-	    this.v_y = 2;
-	else this.v_y = 0;
-	this.x += this.v_x;
-	this.y += this.v_y;
+    update() {
+        if (keyboard.get('D'.charCodeAt(0)))
+            this.v_x = 2;
+        else if (keyboard.get('A'.charCodeAt(0)))
+            this.v_x = -2;
+        else this.v_x = 0;
+        if (keyboard.get('W'.charCodeAt(0)))
+            this.v_y = -2;
+        else if (keyboard.get('S'.charCodeAt(0)))
+            this.v_y = 2;
+        else this.v_y = 0;
+        this.x += this.v_x;
+        this.y += this.v_y;
     }
-    draw()
-    {
-	ctx.fillStyle = "#FFD580";
-	ctx.fillRect(this.x, this.y, 80, 60);
+    draw() {
+        ctx.fillStyle = "#FFD580";
+        ctx.fillRect(this.x, this.y, 80, 60);
     }
 }
-let plr = new Player();
 
-function drawMenu()
-{
+function drawMenu() {
     ctx.fillStyle = "#EEEEEE";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = "black";
     ctx.fillText("Shooter", drawMenu.x, 90);
     if (drawMenu.x + 30 * 3 == canvas.width)
-	drawMenu.step = -2;
+        drawMenu.step = -2;
     else if (drawMenu.x == 0)
-	drawMenu.step = 2;
+        drawMenu.step = 2;
     drawMenu.x += drawMenu.step;
 }
 drawMenu.x = 10;
 drawMenu.step = 2;
 
-function drawGame()
-{
+let plr = new Player();
+function drawGame() {
     plr.update();
-    
+
     ctx.fillStyle = "#0088aa";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     plr.draw();
@@ -76,16 +71,14 @@ function drawGame()
 let state = {
     scene: "menu"
 };
-
-function draw()
-{
+function draw() {
     window.requestAnimationFrame(draw);
     if (keyboard.get(13))
-	state.scene = "game";
+        state.scene = "game";
     if (state.scene == "menu")
-	drawMenu();
+        drawMenu();
     else if (state.scene == "game")
-	drawGame();
+        drawGame();
 }
 
 draw();
